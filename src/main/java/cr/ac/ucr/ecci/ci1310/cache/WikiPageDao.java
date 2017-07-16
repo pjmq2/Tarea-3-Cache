c6d42b6f3e872c067a5fec2284d12cc3b2d3a6f0
Pablo Madrigal <pjmq2@hotmail.com>
2017-07-16 13:31:18 -0600

    Cambios en el app

--- src/main/java/cr/ac/ucr/ecci/ci1310/cache/WikiPageDao.java
+++ src/main/java/cr/ac/ucr/ecci/ci1310/cache/WikiPageDao.java
@@ -4,6 +4,6 @@ package cr.ac.ucr.ecci.ci1310.cache;
  * Created by pjmq2 on 15/07/2017.
  */
 public interface WikiPageDao {
-    public abstract void searchId(String id);
-    public abstract void searchName(String name);
+    public abstract WikiPage searchId(String id);
+    public abstract WikiPage searchName(String name);
 }
